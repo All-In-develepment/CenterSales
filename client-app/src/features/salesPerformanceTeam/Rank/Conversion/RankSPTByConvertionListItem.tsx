fb1c820191583745a919715f0cd353c21258bb09
ronaldocestrela <ronaldo@grupoall.in>
2024-05-31 09:45:45 -0300

    create

--- client-app/src/features/salesPerformanceTeam/Rank/Conversion/RankSPTByConvertionListItem.tsx
+++ client-app/src/features/salesPerformanceTeam/Rank/Conversion/RankSPTByConvertionListItem.tsx
@@ -1,31 +1,79 @@
 import { observer } from "mobx-react-lite";
 import { SalePerformanceTeam } from "../../../../app/models/salePerformanceTeam";
-import { Card, Feed, FeedEvent, Icon } from "semantic-ui-react";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import { CardContent, Typography } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
+import PercentIcon from '@mui/icons-material/Percent';
+import GroupsIcon from '@mui/icons-material/Groups';
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import PriceCheckIcon from '@mui/icons-material/PriceCheck';
 
 interface Props {
   sale: SalePerformanceTeam
 }
 
 export default observer(function RankSPTByConvertionListItem({sale}: Props) {
-    return(
-      <>
-        <Card style={{width: '100%'}}>
-          <Card.Content>
-            <Feed>
-              <FeedEvent>
-                <Feed.Content>
-                  <Feed.Summary>
-                    {sale.sptSellerName}
-                  </Feed.Summary>
-                  <br />
-                  <Feed.Date>
-                    <Icon name="currency" />{sale.sptavgConvertion}
-                  </Feed.Date>
-                </Feed.Content>
-              </FeedEvent>
-            </Feed>
-          </Card.Content>
-        </Card>
-      </>
-    )
+  const Item = styled(Paper)(({}) => ({
+    backgorundColor: 'red',
+    textAlign: 'center',
+    margin: '10px',
+  }));
+
+  return(
+    <Item>
+      <Card style={{width: '100%'}}>
+        <CardContent>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid 
+              container 
+              rowSpacing={2} 
+              spacing={{ xs: 1, sm: 2, md: 3 }} 
+              justifyContent="space-evenly" 
+              alignItems="center"
+            >
+              <Typography variant="h5" gutterBottom>
+                {sale.sptSellerName}
+              </Typography>
+            </Grid>
+            <Grid
+              container 
+              rowSpacing={2} 
+              spacing={{ xs: 1, sm: 2, md: 3 }} 
+              justifyContent="space-evenly" 
+              alignItems="center"
+            >
+              <Typography variant="h5" color="text.secondary" gutterBottom>
+                {sale.sptavgConvertion.toFixed(2)}<PercentIcon name="percent" />
+              </Typography>
+            </Grid>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent="space-evenly" alignItems="center">
+              <Grid>
+                <GroupsIcon name="group" />{sale.sptTotalLeads} {sale.sptTotalLeads > 1 ? 'Leads' : 'Lead'}
+              </Grid>
+              <Grid>
+                <PointOfSaleIcon name="chart line" />{sale.sptTotalSales} {sale.sptTotalSales > 1 ? 'Vendas' : 'Venda'}
+              </Grid>
+              <Grid>
+                <LocalAtmIcon name="money bill alternate outline" />R$ {sale.sptTotalSalesAmont.toFixed(2)}
+              </Grid>
+              <Grid>
+                <HowToVoteIcon />{sale.sptTotalRedeposit + sale.sptTotalRegister} {(sale.sptTotalRedeposit  + sale.sptTotalRegister) > 1 ? 'Depositos' : 'Deposito'}
+              </Grid>
+              <Grid>
+                <PriceCheckIcon />{(sale.sptTotalRedepositAmont + sale.sptTotalRedepositAmont).toFixed(2)}
+              </Grid>
+            </Grid>
+          </Box>
+        </CardContent>
+      </Card>
+    </Item>
+  )
   });
